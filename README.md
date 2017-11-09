75b19b01bdb3b9c3d8ffd90e0062497ca9d6e380
Juan De la Cruz <ejdelacruz@uninorte.edu.co>
2017-11-09 11:25:27 -0500

    programa finalizado, requirements añadidos y actualizacion del readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Para correr el programa:
 
-1. Se abre la terminal
-2. Se navega al directorio donde está el main.py
-3. Estando en el directorio se escribe "python main.py"
+1. Abrir la terminal
+2. Se navega hasta el escritorio del programa
+3. Estando en el escritorio del programa se escribe en la terminal "pip install -r requirements.txt" para instalar las librerias
+4. para correr el programa se escribe "python -m memory_profiler recursividad.py" (Se debe de estar en el escritorio donde esta el archivo recursividad.py)
--- recursividad.py
+++ recursividad.py
@@ -5,45 +5,31 @@ import re
 from decorators import *
 
 
-
-
-@timer
 @profile # Retorna la memoria utilizada por esta funcion y por las funciones ejecutadas dentro de esta
 def MatrizRecursiva(n, m, matriz):
     '''
     Confirma que la matriz generada recursivamente contenga burbujas
     '''
 
-    leer_burbuja = True
-    bubbles = False # Por default la matriz no contiene burbujas
+    leer_coordenada = True
 
     field = [] # Campos de la matriz
+
     generarMatrizRecursiva(matriz, field, n, m, 0, 0)
-
-    while(bubbles == False):
-
-        if(validarBurbujas(matriz, n, m)):   # Valida que la matriz contenga burbujas
-            bubbles = True
-
-        else:
-            matriz = [] # Reinicio la matriz
-            field = [] # Reinicio los campos de la matriz
-            generarMatrizRecursiva(matriz, field, n, m, 0, 0) # Si no contiene burbujas vuelve a generar la matriz
 
     mostrarMatrizRecursiva(matriz, n, m, 0, 0) # Escribe la matriz
 
-    while(leer_burbuja):
-        try:
-            x = int(input("Digite la posicion en X de la burbuja "))
-            y = int(input("Digite la posicion en Y de la burbuja "))
+    while(leer_coordenada):
 
-            leer_burbuja = False
+        x, y = leerCoordenadas(0, 0)
 
-        except Exception:
-            print("Lo siento, solo acepto numeros")
+        if (matriz[x][y] == "x" and validarBurbuja(matriz, x, y,n, m)):
 
-    borradoRecursivo(matriz, x, y,n-1, m-1, False)
-    mostrarMatrizRecursiva(matriz, n, m, 0, 0)
+            borradoRecursivo(matriz, x, y,n-1, m-1, False)
+            mostrarMatrizRecursiva(matriz, n, m, 0, 0)
+            leer_coordenada = False
+        else:
+            print("No hay burbujas en las coordenadas dadas, digite otra coordenada")
 
 @profile
 def MatrizIterativa(n, m, matriz):
@@ -53,88 +39,26 @@ def MatrizIterativa(n, m, matriz):
 
     matriz = generarMatrizIterativa(n, m)
 
-    leer_burbuja = True
-    bubbles = False # Por default la matriz no contiene burbujas
-
-    while(bubbles == False):
-
-        if(validarBurbujas(matriz, n, m)): # Valida que la matriz contenga burbujas
-            bubbles = True
-            
-        else:
-            matriz = generarMatrizIterativa(n, m) # Si no contiene burbujas vuelve a generar la matriz
+    leer_coordenada = True
 
     print('\n'.join([''.join(['{:4}'.format(item) for item in row]) # Escribe la matriz
         for row in matriz]))
 
-      while(leer_burbuja):
-        try:
-            x = int(input("Digite la posicion en X de la burbuja "))
-            y = int(input("Digite la posicion en Y de la burbuja "))
+    while(leer_coordenada):
 
-            leer_burbuja = False
+        x, y = leerCoordenadas(0, 0)
 
-        except Exception:
-            print("Lo siento, solo acepto numeros")
+        if (matriz[x][y] == "x" and validarBurbuja(matriz, x, y, n, m)):
 
-
-def validarBurbujas(matriz, n, m):
-    '''
-    Valido si la matriz contiene burbujas
-    '''
-
-    contieneBurbujas = False
-    cant_burbujas = 0
-
-    for i in range(0, n-1):
-        for j in range(0, m-1):
-
-            # Valido que haya si quiera una burbuja en la matriz iterando en cada posicion posible
-
-            if (i == 0 and j == 0):
-                if (matriz[i+1][j] == "x" or matriz[i][j+1] == "x"):
-                    cant_burbujas += 1
-                
-            if (i == 0 and j != 0 and j != m):
-                if (matriz[i+1][j] == "x" or matriz[i][j+1] == "x" or matriz[i][j-1] == "x"):
-                    cant_burbujas += 1
-
-            if (i == 0 and j == m):
-                if (matriz[i-1][j] == "x" or matriz[i][j-1] == "x"):
-                    cant_burbujas += 1
-
-            if (i == n and j == 0):
-                if (matriz[i-1][j] == "x" or matriz[i][j+1] == "x"):
-                    cant_burbujas += 1
-
-            if(i == n and j != m and j != 0):
-                if (matriz[i][j-1] == "x" or matriz[i][j+1] == "x" or matriz[i-1][j] == "x"):
-                    cant_burbujas += 1
-
-            if(i == n and j == m):
-                if (matriz[i-1][j] == "x" or matriz[i][j-1] == "x"):
-                    cant_burbujas += 1
-
-            if(j == 0 and i != 0 and i != n):
-                if(matriz[i-1][j] == "x" or matriz[i][j+1] == "x" or matriz[i+1][j] == "x"):
-                    cant_burbujas += 1
+            borradoIterativo(matriz, x, y,n-1, m-1)
             
-            if(j == m and i != 0 and i != n):
-                if(matriz[i-1][j] == "x" or matriz[i][j-1] == "x" or matriz[i+1][j] == "x"):
-                    cant_burbujas += 1
+            print('\n'.join([''.join(['{:4}'.format(item) for item in row]) # Escribe la matriz
+            for row in matriz]))
 
-            if(i != 0 and i != n and j != 0 and j != m):
-                if(matriz[i-1][j] == "x" or matriz[i][j+1] == "x" or matriz[i+1][j] == "x" 
-                    or matriz[i][j-1] == "x"):
-                    cant_burbujas += 1
-                
+            leer_coordenada = False
+        else:
+            print("No hay burbujas en las coordenadas dadas, digite otra coordenada")
 
-    if (cant_burbujas > 0):
-        # Si hay mas de 1 burbuja la matriz puede continuar.
-        return True
-
-    else:
-        return False
 
 
 @timer
@@ -155,7 +79,7 @@ def generarMatrizIterativa(n, m):
     return matriz
 
 
-
+@timer
 def generarMatrizRecursiva(matriz ,field, n, m, i, j):
     '''
     Genera una matriz de manera recursiva
@@ -173,7 +97,7 @@ def generarMatrizRecursiva(matriz ,field, n, m, i, j):
     field = [] # reinicio el campo para que cuando salga sea 0 
 
 
-
+@timer
 def mostrarMatrizRecursiva(matriz, n, m, i, j):
     '''
     Muestra la matriz de manera recursiva
@@ -196,42 +120,45 @@ def borradoRecursivo(matriz, x, y, n , m , noHayX):
         matriz[x][y] = "o"
     else:
         if (x != 0 and x != n and y != 0 and y != m):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
 
-            if(matriz[x][y+1] == "x"):
+            if(validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
 
-            if(matriz[x+1][y] == "x"):
+            if(validarAbajo(matriz, x, y)):
                 matriz[x+1][y] = "o"
                 borradoRecursivo(matriz, x+1, y, n, m, noHayX)
 
-            if(matriz[x][y-1] == "x"):
+            if(validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
-
             else:
                 noHayX = True
 
         elif (x == 0 and y == 0):
-            if(matriz[x][y+1]) == "x":
+            if(validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
+
+            if(validarAbajo(matriz, x, y)):
+                matriz[x+1][y] = "o"
+                borradoRecursivo(matriz, x+1, y,n, m, noHayX )
             else:
                 noHayX = True
 
         elif (x == 0 and y < m and y != 0):
-            if(matriz[x][y+1] == "x"):
+            if(validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
             
-            if(matriz[x+1][y] == "x"):
+            if(validarAbajo(matriz, x, y)):
                 matriz[x+1][y] = "o"
                 borradoRecursivo(matriz, x+1, y, n, m, noHayX)
                 
-            if(matriz[x][y-1] == "x"):
+            if(validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
             else:
@@ -239,7 +166,11 @@ def borradoRecursivo(matriz, x, y, n , m , noHayX):
 
 
         elif (x == 0 and y == m):
-            if(matriz[x][y-1] == "x"):
+            if(validarAbajo(matriz, x, y)):
+                matriz[x+1][y] = "o"
+                borradoRecursivo(matriz, x+1, y, n, m, noHayX)
+            
+            if(validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
             else:
@@ -247,26 +178,26 @@ def borradoRecursivo(matriz, x, y, n , m , noHayX):
 
 
         elif(y == m and x != 0 and x != n):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
 
-            if (matriz[x+1][y] == "x"):
+            if (validarAbajo(matriz, x, y)):
                 matriz[x+1][y] = "o"
                 borradoRecursivo(matriz, x+1, y, n, m , noHayX)
 
-            if (matriz[x][y-1] == "x"):
+            if (validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
             else:
                 noHayX = True
 
         elif(y == m and x == n):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
             
-            if(matriz[x][y-1] == "x"):
+            if(validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
             else:
@@ -274,26 +205,26 @@ def borradoRecursivo(matriz, x, y, n , m , noHayX):
 
         
         elif(x == n and y != 0 and y != m):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
 
-            if (matriz[x][y+1] == "x"):
+            if (validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
             
-            if (matriz[x][y-1] == "x"):
+            if (validarIzquierda(matriz, x, y)):
                 matriz[x][y-1] = "o"
                 borradoRecursivo(matriz, x, y-1, n, m, noHayX)
             else:
                 noHayX = True
 
         elif(x == n and y == 0):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
             
-            if(matriz[x][y+1] == "x"):
+            if(validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
             else:
@@ -301,28 +232,261 @@ def borradoRecursivo(matriz, x, y, n , m , noHayX):
 
 
         elif (y == 0 and x != n and x != 0):
-            if(matriz[x-1][y] == "x"):
+            if(validarArriba(matriz, x, y)):
                 matriz[x-1][y] = "o"
                 borradoRecursivo(matriz, x-1, y, n, m, noHayX)
             
-            if(matriz[x][y+1] == "x"):
+            if(validarDerecha(matriz, x, y)):
                 matriz[x][y+1] = "o"
                 borradoRecursivo(matriz, x, y+1, n, m, noHayX)
 
-            if(matriz[x+1][y] == "x"):
+            if(validarAbajo(matriz, x, y)):
                 matriz[x+1][y] = "o"
                 borradoRecursivo(matriz, x+1, y, n, m, noHayX)
             else:
                 noHayX = True
 
 
+@timer
+def borradoIterativo(matriz, x, y, n, m):
+    coordenadas = []
+    cont = 0
+    contPos = 0
+    hayX = True
+    print (x)
+    print(y)
+    while(hayX):
+        
+        if (x != 0 and x != n and y != 0 and y != m):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+
+            if(validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+
+            if(validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x+1) + "," + str(y))
+
+            if(validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
+
+        elif (x == 0 and y == 0):
+            if(validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+
+            if(validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x+1) + "," + str(y))
+
+
+        elif (x == 0 and y != m and y != 0):
+            if(validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+            
+            if(validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x+1) + "," + str(y))
+                
+            if(validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
 
 
 
+        elif (x == 0 and y == m):
+            if(validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x+1) + "," + str(y))
+            
+            if(validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
 
-#def verificarAdyacente(matriz, x, y, n, m):
-#    if ((x < 0 or y < 0) or (x > n and y >m) )
 
+        elif(y == m and x != 0 and x != n):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+
+            if (validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x+1) + "," + str(y))
+
+            if (validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
+
+
+        elif(y == m and x == n):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+            
+            if(validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
+
+        
+        elif(x == n and y != 0 and y != m):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+
+            if (validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+            
+            if (validarIzquierda(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y-1))
+
+
+        elif(x == n and y == 0):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+            
+            if(validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+
+
+        elif (y == 0 and x != n and x != 0):
+            if(validarArriba(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+            
+            if(validarDerecha(matriz, x, y)):
+                coordenadas.append(str(x) + "," + str(y+1))
+
+            if(validarAbajo(matriz, x, y)):
+                coordenadas.append(str(x-1) + "," + str(y))
+
+        matriz[x][y] = "o"
+        if (len(coordenadas) > 0):
+            auxX = coordenadas[0]
+            auxY = coordenadas[0]
+            auxX = int(auxX[0])
+            auxY = int(auxY[2])
+            x = auxX
+            y = auxY
+            del coordenadas[0]
+        else:
+            hayX = False
+
+        
+
+def validarArriba(matriz, x, y):
+    return matriz[x-1][y] == "x"
+
+
+def validarAbajo(matriz, x, y):
+    return matriz[x+1][y] == "x"
+
+
+def validarDerecha(matriz, x, y):
+    return matriz[x][y+1] == "x"
+
+
+def validarIzquierda(matriz, x, y):
+    return matriz[x][y-1] == "x"
+
+
+def leerCoordenadas(x, y):
+    leer_burbuja = True
+
+    while(leer_burbuja):
+            try:
+                x = int(input("Digite la posicion en X de la burbuja "))
+                y = int(input("Digite la posicion en Y de la burbuja "))
+
+                leer_burbuja = False
+                return x, y
+            except Exception:
+                print("Lo siento, solo acepto numeros")
+        
+
+def validarBurbuja(matriz, x, y, n, m):
+    up = False
+    right = False
+    down = False
+    left = False
+    if (x != 0 and x != n and y != 0 and y != m):
+            if(validarArriba(matriz, x, y)):
+                up = True
+
+            if(validarDerecha(matriz, x, y)):
+                right = True
+
+            if(validarAbajo(matriz, x, y)):
+                down = True
+
+            if(validarIzquierda(matriz, x, y)):
+                left = True
+
+    elif (x == 0 and y == 0):
+        if(validarDerecha(matriz, x, y)):
+            right = True
+
+        if(validarAbajo(matriz, x, y)):
+            down = True
+
+
+    elif (x == 0 and y != m and y != 0):
+        if(validarDerecha(matriz, x, y)):
+            right = True
+        
+        if(validarAbajo(matriz, x, y)):
+            down = True
+            
+        if(validarIzquierda(matriz, x, y)):
+            left = True
+
+
+    elif (x == 0 and y == m):
+        if(validarAbajo(matriz, x, y)):
+            down = True
+        
+        if(validarIzquierda(matriz, x, y)):
+            left = True
+
+
+    elif(y == m and x != 0 and x != n):
+        if(validarArriba(matriz, x, y)):
+            up = True
+
+        if (validarAbajo(matriz, x, y)):
+            down = True
+
+        if (validarIzquierda(matriz, x, y)):
+            left = True
+
+
+    elif(y == m and x == n):
+        if(validarArriba(matriz, x, y)):
+            up = True
+        
+        if(validarIzquierda(matriz, x, y)):
+            left = True
+
+    
+    elif(x == n and y != 0 and y != m):
+        if(validarArriba(matriz, x, y)):
+            up = True
+
+        if (validarDerecha(matriz, x, y)):
+            right = True
+        
+        if (validarIzquierda(matriz, x, y)):
+            left = True
+
+
+    elif(x == n and y == 0):
+        if(validarArriba(matriz, x, y)):
+            up = True
+        
+        if(validarDerecha(matriz, x, y)):
+            right = True
+
+
+    elif (y == 0 and x != n and x != 0):
+        if(validarArriba(matriz, x, y)):
+            up = True
+        
+        if(validarDerecha(matriz, x, y)):
+            right = True
+
+        if(validarAbajo(matriz, x, y)):
+            down = True
+
+    if(up or down or left or right):
+        return True
+    else:
+        return False
 
 def main():
 
